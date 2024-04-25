68242f5b92e46ccbf466baae9eafa7a6cf0807b6
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-25 21:49:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// 37.	Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size = "Large", message = " Life is a beautiful ride!") {
+    console.log(`I am providing a ${size} T.shirt with a pretty quote "${message}". `);
+}
+make_shirt("Large");
+make_shirt("Medium", "Live more, worry less.");
+make_shirt("Small", "Don't worry. Be happy.");
